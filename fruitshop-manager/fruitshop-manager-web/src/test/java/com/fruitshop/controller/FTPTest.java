228e472dc45fca48fef24a4758ecc1cdecf0bbbc
Demo-du <1556141770@qq.com>
2017-09-13 09:26:48 +0800

    bug修复，ftp路径权限造成

---
+++ fruitshop-manager/fruitshop-manager-web/src/test/java/com/fruitshop/controller/FTPTest.java
@@ -0,0 +1,44 @@
+package com.fruitshop.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import org.apache.commons.net.ftp.FTP;
+import org.apache.commons.net.ftp.FTPClient;
+import org.junit.Test;
+
+import com.fruitshop.common.utils.FtpUtil;
+
+//import com.fruitshop.common.utils.FtpUtil;
+
+public class FTPTest {
+
+	@Test
+	public void testFtpClient() throws Exception {
+		//创建一个FtpClient对象
+		FTPClient ftpClient = new FTPClient();
+		//创建ftp连接。默认是21端口
+		ftpClient.connect("127.0.0.1", 21);
+		//登录ftp服务器，使用用户名和密码
+		ftpClient.login("djjftp", "dujianjian");
+		//上传文件。
+		//读取本地文件
+		FileInputStream inputStream = new FileInputStream(new File("/home/dujianjian/1.jpg"));
+		//设置上传的路径
+		ftpClient.changeWorkingDirectory("/home/ftp/image");
+		//修改上传文件/的格式
+		ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+		//第一个参数：服务器端文档名
+		//第二个参数：上传文档的inputStream
+		ftpClient.storeFile("hello1.jpg", inputStream);
+		//关闭连接
+		ftpClient.logout();
+	}
+	
+	@Test
+	public void testFtpUtil() throws Exception {
+		FileInputStream inputStream = new FileInputStream(new File("/home/dujianjian/1.jpg"));
+		FtpUtil.uploadFile("localhost", 21, "djjftp", "dujianjian", "/home/ftp/", "/2017/09/012", "1.jpg", inputStream);
+		
+	}
+}
