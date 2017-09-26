72fa6d2723ba5f6e87b964016a9b0edbbf094c28
Demo-du <1556141770@qq.com>
2017-09-26 16:12:14 +0800

    水果参数展示controller

---
+++ fruitshop-manager/fruitshop-manager-web/src/main/java/com/fruitshop/controller/ItemParamItemController.java
@@ -0,0 +1,27 @@
+package com.fruitshop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.fruitshop.service.ItemParamItemService;
+
+/**
+ * 展示商品规格参数
+ * @version 1.0
+ */
+@Controller
+public class ItemParamItemController {
+
+	@Autowired
+	private ItemParamItemService itemParamItemService;
+	
+	@RequestMapping("/showitem/{itemId}")
+	public String showItemParam(@PathVariable Long itemId, Model model) {
+		String string = itemParamItemService.getItemParamByItemId(itemId);
+		model.addAttribute("itemParam", string);
+		return "item";
+	}
+}
