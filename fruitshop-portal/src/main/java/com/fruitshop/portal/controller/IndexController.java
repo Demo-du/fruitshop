25743dcacccea8eff301a39c66f0e11803e72c5c
Demo-du <1556141770@qq.com>
2017-09-27 15:19:11 +0800

    门户工程配置

---
+++ fruitshop-portal/src/main/java/com/fruitshop/portal/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.fruitshop.portal.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+	
+
+	@RequestMapping("/index")
+	public String showIndex(Model model) {
+		return "index";
+	}
+	
+}
