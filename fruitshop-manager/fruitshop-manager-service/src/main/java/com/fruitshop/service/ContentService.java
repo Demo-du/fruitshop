e2847b9cbe0743647076f38a0f8dddaf3d5ce1db
Demo-du <1556141770@qq.com>
2017-10-05 17:13:35 +0800

    内容添加servise

--- fruitshop-manager/fruitshop-manager-service/src/main/java/com/fruitshop/service/ContentService.java
+++ fruitshop-manager/fruitshop-manager-service/src/main/java/com/fruitshop/service/ContentService.java
@@ -3,6 +3,7 @@ package com.fruitshop.service;
 import com.fruitshop.common.pojo.FruitshopResult;
 import com.fruitshop.pojo.TbContent;
 
+
 public interface ContentService {
 
 	FruitshopResult insertContent(TbContent content);
---
+++ fruitshop-manager/fruitshop-manager-service/src/main/java/com/fruitshop/service/impl/ContentServiceImpl.java
@@ -0,0 +1,47 @@
+package com.fruitshop.service.impl;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.fruitshop.common.pojo.FruitshopResult;
+
+import com.fruitshop.mapper.TbContentMapper;
+import com.fruitshop.pojo.TbContent;
+import com.fruitshop.service.ContentService;
+
+/**
+ * 内容管理
+ * @version 1.0
+ */
+@Service
+public class ContentServiceImpl implements ContentService {
+
+	@Autowired
+	private TbContentMapper  contentMapper;
+	@Value("${REST_BASE_URL}")
+	private String REST_BASE_URL;
+	@Value("${REST_CONTENT_SYNC_URL}")
+	private String REST_CONTENT_SYNC_URL;
+	
+	
+	@Override
+	public FruitshopResult insertContent(TbContent content) {
+		//补全pojo内容
+		content.setCreated(new Date());
+		content.setUpdated(new Date());
+		contentMapper.insert(content);
+		
+		//添加缓存同步逻辑
+		/*try {
+			HttpClientUtil.doGet(REST_BASE_URL + REST_CONTENT_SYNC_URL + content.getCategoryId());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		*/
+		return FruitshopResult.ok();
+	}
+
+}
