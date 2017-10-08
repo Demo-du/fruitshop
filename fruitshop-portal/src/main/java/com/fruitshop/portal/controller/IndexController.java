b797d8144069ee4b406161f8426aaf008617e53e
Demo-du <1556141770@qq.com>
2017-10-08 16:02:21 +0800

    展示controller

--- fruitshop-portal/src/main/java/com/fruitshop/portal/controller/IndexController.java
+++ fruitshop-portal/src/main/java/com/fruitshop/portal/controller/IndexController.java
@@ -7,12 +7,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.fruitshop.portal.service.ContentService;
+
 @Controller
 public class IndexController {
 	
-
+	@Autowired
+	private ContentService contentService;
 	@RequestMapping("/index")
 	public String showIndex(Model model) {
+		String adJson = contentService.getContentList();
+		model.addAttribute("ad1", adJson);
+		
 		return "index";
 	}
 	
