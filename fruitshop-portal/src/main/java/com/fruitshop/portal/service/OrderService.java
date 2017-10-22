4ff9166f6cc3d2858a3c1a85fd12f5de49396a42
Demo-du <1556141770@qq.com>
2017-10-22 22:14:18 +0800

    添加购物车service

---
+++ fruitshop-portal/src/main/java/com/fruitshop/portal/service/OrderService.java
@@ -0,0 +1,8 @@
+package com.fruitshop.portal.service;
+
+import com.fruitshop.portal.pojo.Order;
+
+public interface OrderService {
+
+	String createOrder(Order order);
+}
---
+++ fruitshop-portal/src/main/java/com/fruitshop/portal/service/impl/OrderServiceImpl.java
@@ -0,0 +1,41 @@
+package com.fruitshop.portal.service.impl;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.fruitshop.common.pojo.FruitshopResult;
+import com.fruitshop.common.utils.HttpClientUtil;
+import com.fruitshop.common.utils.JsonUtils;
+import com.fruitshop.portal.pojo.Order;
+import com.fruitshop.portal.service.OrderService;
+
+/**
+ * 订单处理Service
+ * @version 1.0
+ */
+@Service
+public class OrderServiceImpl implements OrderService {
+	
+	@Value("${ORDER_BASE_URL}")
+	private String ORDER_BASE_URL;
+	@Value("${ORDER_CREATE_URL}")
+	private String ORDER_CREATE_URL;
+	
+
+	@Override
+	public String createOrder(Order order) {
+		//调用创建订单服务之前补全用户信息。
+		//从cookie中后取TT_TOKEN的内容，根据token调用sso系统的服务根据token换取用户信息。
+		
+		//调用taotao-order的服务提交订单。
+		String json = HttpClientUtil.doPostJson(ORDER_BASE_URL + ORDER_CREATE_URL, JsonUtils.objectToJson(order));
+		//把json转换成taotaoResult
+		TaotaoResult taotaoResult = TaotaoResult.format(json);
+		if (taotaoResult.getStatus() == 200) {
+			Object orderId = taotaoResult.getData();
+			return orderId.toString();
+		}
+		return "";
+	}
+
+}
